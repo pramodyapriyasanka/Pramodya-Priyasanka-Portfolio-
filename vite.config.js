69392cb9e0d935294dd5665797688cba8745da30
pramodya priyasanka <143013116+pramodyapriyasanka@users.noreply.github.com>
2026-05-18 18:43:07 +0530

    Fix: set base path to root for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === "production" ? '/Pramodya-PriyasankaPortfolio/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "./src"),
